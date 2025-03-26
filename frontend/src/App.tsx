b9415d72c4222f118a3013fc31b458920900ed81
Aditya <adityapsingh559@gmail.com>
2025-03-26 18:40:32 +0530

    added  viedocall functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Landing from "./components/Landing";
-import Room from "./components/Room";
+import { Landing } from "./components/Landing";
+import { Room } from "./components/Room";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/" element={<Room />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
